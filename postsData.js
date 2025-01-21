d2b13f94c93363bcb658c754ea99056eb35e806b
Ortello7 <domenicoortello00@gmail.com>
2025-01-21 15:53:50 +0100

    created file app.js

---
+++ postsData.js
@@ -0,0 +1,34 @@
+let posts =    [
+    {
+      title: "Viaggio nelle Alpi",
+      content: "Un'esperienza unica tra le cime innevate e i paesaggi mozzafiato delle Alpi.",
+      image: `http://localhost:${port}/IMG/ciambellone.jpeg`,
+      tags: ["natura", "montagna", "viaggio", "avventura"]
+    },
+    {
+      title: "Il fascino della cucina italiana",
+      content: "Scopri le tradizioni culinarie italiane, dalla pasta fatta in casa alla pizza napoletana.",
+      image: `http://localhost:${port}/IMG/cracker_barbabietola.jpeg`,
+      tags: ["cucina", "Italia", "tradizioni", "gastronomia"]
+    },
+    {
+      title: "Tecnologie emergenti nel 2025",
+      content: "Un approfondimento sulle innovazioni tecnologiche che stanno cambiando il nostro mondo.",
+      image: `http://localhost:${port}/IMG/pane_fritto_dolce.jpeg`,
+      tags: ["tecnologia", "innovazione", "futuro", "AI"]
+    },
+    {
+      title: "I benefici dello yoga",
+      content: "Come lo yoga può migliorare il tuo benessere fisico e mentale.",
+      image: `http://localhost:${port}/IMG/pasta_barbabietola.jpeg`,
+      tags: ["yoga", "benessere", "salute", "meditazione"]
+    },
+    {
+      title: "Guida ai migliori libri del 2024",
+      content: "Una selezione dei libri più apprezzati dell'anno scorso per tutti i tipi di lettori.",
+      image: `http://localhost:${port}/IMG/torta_paesana.jpeg`,
+      tags: ["libri", "lettura", "cultura", "recensioni"]
+    }
+  ]
+
+  module.exports = posts;
